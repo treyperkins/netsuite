14f83e037ac9ff82b95ee72a9172d5402e7ef570
Trey Perkins <51243+treyperkins@users.noreply.github.com>
2026-06-22 15:00:42 -0500

    URI encode all the oauth components to be safe

--- lib/netsuite/passports/token.rb
+++ lib/netsuite/passports/token.rb
@@ -26,16 +26,17 @@ module NetSuite
       end
 
       def header
-        {
-          "Oauth_realm" => account,
-          "oauth_consumer_key" => consumer_key,
-          "oauth_token" => token_id,
-          "oauth_signature_method" => "HMAC-SHA256",
-          "oauth_timestamp" => timestamp,
-          "oauth_nonce" => nonce,
-          "oauth_version" => "1.0",
-          "oauth_signature" => signature,
+        parts = {
+          "realm" => URI.encode_uri_component(account),
+          "oauth_consumer_key" => URI.encode_uri_component(consumer_key),
+          "oauth_token" => URI.encode_uri_component(token_id),
+          "oauth_signature_method" => URI.encode_uri_component("HMAC-SHA256"),
+          "oauth_timestamp" => URI.encode_uri_component(timestamp),
+          "oauth_nonce" => URI.encode_uri_component(nonce),
+          "oauth_version" => URI.encode_uri_component("1.0"),
+          "oauth_signature" => URI.encode_uri_component(signature),
         }
+        "OAuth " + parts.map { |k, v| "#{k}=\"#{v}\"" }.join(", ")
       end
 
       private
